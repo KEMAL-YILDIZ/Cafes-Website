ad38c7733f4e9bd77a6284a04144593ba214bf4f
KEMAL-YILDIZ <143597526+KEMAL-YILDIZ@users.noreply.github.com>
2023-09-01 12:33:55 +0300

    Added the delete button +
    Added the delete button and made it work and cleaned up some stuff

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, URLField, TimeField
+from wtforms import StringField, SubmitField, SelectField, URLField
 from wtforms.validators import DataRequired, URL
 from flask_sqlalchemy import SQLAlchemy
-import csv
 
-'''
-On Windows type:
-python -m pip install -r requirements.txt
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -18,10 +13,11 @@ bootstrap = Bootstrap5(app)
 
 db = SQLAlchemy(app)
 
+
 class CafesData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
-    location = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    location = db.Column(db.String(250), unique=True, nullable=False)
     open = db.Column(db.String(250), nullable=False)
     close = db.Column(db.String(250), nullable=False)
     coffee = db.Column(db.String(250), nullable=False)
@@ -37,19 +33,11 @@ class CafeForm(FlaskForm):
     coffee = SelectField("Coffee Rating",
                          choices=["☕", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
     wifi = SelectField("WIFI Strength Rating",
-                       choices=["💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+                       choices=["📡", "📡📡", "📡📡📡", "📡📡📡📡", "📡📡📡📡📡"], validators=[DataRequired()])
     power = SelectField("Power Socket Availability",
                         choices=["🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-# e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
 # all Flask routes below
 @app.route("/")
@@ -68,22 +56,22 @@ def add_cafe():
             db.session.add(new_cafe)
             db.session.commit()
             return redirect(url_for("cafes"))
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
+    return render_template("add.html", form=form)
 
 
 @app.route('/cafes')
 def cafes():
     result = db.session.execute(db.Select(CafesData).order_by(CafesData.name)).scalars()
-    print(result)
-    return render_template('cafes.html', cafes=result)
+    return render_template("cafes.html", cafes=result)
 
 
 @app.route('/delete')
 def delete():
-    pass
+    id = request.args.get("id")
+    cafe_to_del = db.session.execute(db.Select(CafesData).where(CafesData.id == id)).scalar()
+    db.session.delete(cafe_to_del)
+    db.session.commit()
+    return redirect(url_for("cafes"))
 
 
 if __name__ == '__main__':
